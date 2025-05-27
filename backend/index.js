3f0a453aa46de449724092a7046e40b8eb375013
NitinSapra44 <codedev4411@gmail.com>
2025-05-27 22:35:53 +0530

    Update link for store

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ import { router as orderRouter } from "./routes/order.js";
 import { router as paymentRouter } from "./routes/payment.js";
 
 // CORS Configuration
-const allowedOrigins = ["http://localhost:5173", "https://againstthetribe-admin.onrender.com"];
+const allowedOrigins = ["https://againstthetribe-shop.onrender.com", "https://againstthetribe-admin.onrender.com"];
 app.use(
   cors({
     origin: (origin, callback) => {
